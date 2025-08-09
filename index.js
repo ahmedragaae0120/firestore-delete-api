3ac6e7b5ba970f021858af30da5cde424ede6d9c
اسمك <ahmedbasha01208@gmail.com>
2025-08-09 14:11:42 +0300

    change

--- index.js
+++ index.js
@@ -35,4 +35,8 @@ app.delete("/delete", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Server is running 🚀');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
